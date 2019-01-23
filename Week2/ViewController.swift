fed32836d9e5eb73bd8ff8e3c631717db36733e8
Mark Lee <marklee@marks-macbook-pro-2.local>
2019-01-23 16:42:58 -0500

    Initial Commit

---
+++ Week2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by Mark Lee on 1/23/19.
+//  Copyright © 2019 Mark Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
